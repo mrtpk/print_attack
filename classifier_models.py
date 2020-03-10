372bdf0a9ecdbbc904cbc1f65bafddaeb892342f
Thomas Paul <nivintpaul@gmail.com>
2020-03-10 12:38:46 +0530

    two classifiers for detection

---
+++ classifier_models.py
@@ -0,0 +1,40 @@
+"""
+Collection of classifier models.
+Different architectures are trained
+on face embeddings.
+"""
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import Dense,Dropout,Softmax,Flatten,Activation,BatchNormalization
+
+def get_classifier(name, load_pretrained):
+    """
+    Returns classifier
+    """
+    AVAILABLE_CLASSIFIERS = {
+    "tiny_2_layer" : get_tiny_2_layer,
+    "2_layer_dense" : get_2_layer_dense
+    }
+    path_save_model = "./data/models/classifier/{}.h5"
+    if load_pretrained:
+        return load_model(path_save_model.format(name))
+    return AVAILABLE_CLASSIFIERS[name]()
+
+def get_tiny_2_layer():
+    model=Sequential(name="tiny_2_layer")
+    model.add(Dense(units=4,input_dim=(128), kernel_initializer='glorot_uniform'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(units=1, kernel_initializer='he_uniform'))
+    model.add(Activation('sigmoid'))
+    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    return model
+
+def get_2_layer_dense():
+    model=Sequential(name="2_layer_dense")
+    model.add(Dense(units=64,input_dim=(128), kernel_initializer='glorot_uniform'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(units=1, kernel_initializer='he_uniform'))
+    model.add(Activation('sigmoid'))
+    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    return model
---
+++ data_generator.py
@@ -0,0 +1,73 @@
+"""
+Data generator module for face
+embeddings
+"""
+import numpy as np
+import tensorflow as tf
+
+class DataGenerator(tf.keras.utils.Sequence):
+    """
+    Custom data generator
+    """
+    def __init__(self, dataset, batch_size=8, shuffle=True):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        """
+        Returns number of batches in the dataset
+        """
+        return int(np.floor(len(self.dataset) / self.batch_size))
+
+    def __getitem__(self, index):
+        """
+        Returns one batch
+        """
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        _tmp = [self.dataset[k] for k in indexes]
+        return self.convert(_tmp)
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.dataset))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+    
+    def get_xy(self, record):
+        return np.load(record[0]), record[1]
+    
+    def convert(self, records):
+        x, y = [], []
+        for record in records:
+            _x, _y = self.get_xy(record)
+            x.append(_x)
+            y.append(_y)
+        return np.array(x), np.array(y)
+
+def load_all(records):
+    """
+    Loads all records to memory.
+    Alternative to data generator class.
+    """
+    x, y = [], []
+    for record in records:
+        _x, _y = np.load(record[0]), record[1]
+        x.append(_x)
+        y.append(_y)
+    return np.array(x), np.array(y)
+
+if __name__ == "__main__":
+    from data_process import pickle_read
+    train_set = pickle_read("./data/processed/train.pkl")
+    valid_set = pickle_read("./data/processed/valid.pkl")
+    test_set = pickle_read("./data/processed/test.pkl")
+    # data generator usage
+    gen_train = DataGenerator(dataset=train_set, batch_size=128, shuffle=True)
+    gen_valid = DataGenerator(dataset=valid_set, batch_size=128, shuffle=True)
+    gen_test = DataGenerator(dataset=test_set, batch_size=128)
+    # data generator alternate
+    x_train, y_train = load_all(train_set)
+    x_valid, y_valid = load_all(valid_set)
+    x_test, y_test = load_all(test_set)
